e655a9ae008db56f8bed8a066e220a9c96864246
Dmitry Kekelidze <dmitry.kekelidze@cern.ch>
2025-04-04 20:47:18 +0200

    Add KrakenCsvService to parse OHLC data from CSV files
    Implement a service to read and parse Kraken OHLC data from a CSV file into TA4J Bar objects. This includes validation for rows and handles potential IOExceptions.

---
+++ src/main/java/ch/kekelidze/krakentrader/api/service/KrakenCsvService.java
@@ -0,0 +1,64 @@
+package ch.kekelidze.krakentrader.api.service;
+
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import org.ta4j.core.BaseBar;
+import org.ta4j.core.Bar;
+import org.ta4j.core.num.DecimalNum;
+
+@Slf4j
+@Service
+public class KrakenCsvService {
+
+  /**
+   * Kraken’s OHLC Format: Each candle is an array [time, open, high, low, close, volume, count].
+   * @return parsed historical data file
+   */
+  public List<Bar> readCsvFile() {
+    List<Bar> bars = new ArrayList<>();
+    String filePath = "data/XRPUSD_60.csv";
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        String[] fields = line.split(",");
+        if (fields.length != 7) {
+          log.warn("Invalid row in CSV file: {}", line);
+          continue;
+        }
+
+        long epochTime = Long.parseLong(fields[0]);
+        var open = DecimalNum.valueOf(fields[1]);
+        var high = DecimalNum.valueOf(fields[2]);
+        var low = DecimalNum.valueOf(fields[3]);
+        var close = DecimalNum.valueOf(fields[4]);
+        var volume = DecimalNum.valueOf(fields[6]);
+
+        Bar bar = BaseBar.builder(DecimalNum::valueOf, Number.class)
+            .timePeriod(Duration.ofMinutes(60))
+            .endTime(ZonedDateTime.ofInstant(
+                Instant.ofEpochSecond(epochTime),
+                ZoneId.systemDefault()))
+            .openPrice(open)
+            .highPrice(high)
+            .lowPrice(low)
+            .closePrice(close)
+            .volume(volume)
+            .build();
+        bars.add(bar);
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to read CSV file: " + filePath, e);
+    }
+    return bars;
+  }
+}
