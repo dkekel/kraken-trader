9c304035bb677ea9b00b3dc865d42c73bcc82367
Dmitry Kekelidze <dmitry.kekelidze@cern.ch>
2025-04-04 20:50:55 +0200

    Refactor optimization logic and add ML model training.
    Introduced a new `optimizeMLModel` method for training and saving ML models using CSV data. Deprecated existing `optimizeAndTrade` logic and added placeholders for future model validation workflows.

--- src/main/java/ch/kekelidze/krakentrader/KrakenTraderApplication.java
+++ src/main/java/ch/kekelidze/krakentrader/KrakenTraderApplication.java
@@ -1,6 +1,7 @@
 package ch.kekelidze.krakentrader;
 
 import ch.kekelidze.krakentrader.api.service.KrakenApiService;
+import ch.kekelidze.krakentrader.api.service.KrakenCsvService;
 import ch.kekelidze.krakentrader.indicator.optimize.Optimizer;
 import ch.kekelidze.krakentrader.trade.service.TradeService;
 import java.io.File;
@@ -17,9 +18,36 @@ public class KrakenTraderApplication {
   public static void main(String[] args) throws IOException {
     var application = SpringApplication.run(KrakenTraderApplication.class, args);
     var krakenApi = application.getBean(KrakenApiService.class);
+    var krakenCsvService = application.getBean(KrakenCsvService.class);
     var tradeService = application.getBean(TradeService.class);
     var optimizer = application.getBean("walkForwardOptimizer", Optimizer.class);
-    optimizeAndTrade(krakenApi, tradeService, optimizer);
+    optimizeMLModel(krakenCsvService, optimizer);
+    //optimizeAndValidate(krakenCsvService, tradeService, optimizer);
+//    optimizeAndTrade(krakenApi, tradeService, optimizer);
+  }
+
+  private static void optimizeMLModel(KrakenCsvService krakenCsvService, Optimizer optimizer)
+      throws IOException {
+    var historicalData = krakenCsvService.readCsvFile();
+    var modelFile = new File("model_v4.h5");
+    if (!modelFile.exists()) {
+      var model = optimizer.trainModel(historicalData);
+      model.save(modelFile);
+    }
+  }
+
+  private static void optimizeAndValidate(KrakenCsvService krakenCsvService,
+      TradeService tradeService, Optimizer optimizer) throws IOException {
+    var coin = "XRPUSD";
+    var historicalData = krakenCsvService.readCsvFile();
+    var optimizeParameters = optimizer.optimizeParameters(historicalData);
+    log.info("Optimised strategy: {}", optimizeParameters);
+
+    // 30% validation
+    int trainingSize = (int) (historicalData.size() * 0.7);
+    var validationData = historicalData.subList(trainingSize, historicalData.size());
+
+    tradeService.executeStrategy(validationData, optimizeParameters);
   }
 
   private static void optimizeAndTrade(KrakenApiService krakenApiService,
@@ -35,10 +63,5 @@ public class KrakenTraderApplication {
 
     var optimizeParameters = optimizer.optimizeParameters(historicalData);
     log.info("Optimised strategy: {}", optimizeParameters);
-
-    int trainingSize = (int) (historicalData.size() * 0.7); // 30% validation
-    var validationData = historicalData.subList(trainingSize, historicalData.size());
-
-    tradeService.executeStrategy(validationData, optimizeParameters);
   }
 }
