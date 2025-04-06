36076c33463f2a93057b6145df95bfc67d836d34
Dmitry Kekelidze <dmitry.kekelidze@cern.ch>
2025-04-06 12:23:16 +0200

    Add MultiIndexMomentumStrategy for trading logic
    Introduced a new strategy combining multiple indicators (MACD, MFI, RSI) to determine buy and sell signals. Includes risk management to ensure stop-loss and take-profit thresholds. This enhances the trading system's flexibility and decision-making accuracy.

---
+++ src/main/java/ch/kekelidze/krakentrader/strategy/MultiIndexMomentumStrategy.java
@@ -0,0 +1,45 @@
+package ch.kekelidze.krakentrader.strategy;
+
+import ch.kekelidze.krakentrader.indicator.MFIIndicator;
+import ch.kekelidze.krakentrader.indicator.MovingAverageDivergenceIndicator;
+import ch.kekelidze.krakentrader.indicator.RiskManagementIndicator;
+import ch.kekelidze.krakentrader.indicator.RsiIndicator;
+import ch.kekelidze.krakentrader.indicator.optimize.configuration.StrategyParameters;
+import java.util.List;
+import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.ta4j.core.Bar;
+
+@Component
+@RequiredArgsConstructor
+public class MultiIndexMomentumStrategy implements Strategy {
+
+  private final MovingAverageDivergenceIndicator movingAverageDivergenceIndicator;
+  private final MFIIndicator mfiIndicator;
+  private final RsiIndicator rsiIndicator;
+  private final RiskManagementIndicator riskManagementIndicator;
+
+  @Override
+  public boolean shouldBuy(List<Bar> data, StrategyParameters params) {
+    return Stream.of(movingAverageDivergenceIndicator, mfiIndicator, rsiIndicator)
+        .allMatch(indicator -> indicator.isBuySignal(data, params));
+  }
+
+  @Override
+  public boolean shouldSell(List<Bar> data, double entryPrice, StrategyParameters params) {
+    return Stream.of(movingAverageDivergenceIndicator, mfiIndicator, rsiIndicator)
+        .allMatch(indicator -> indicator.isSellSignal(data, entryPrice, params))
+        || riskManagementIndicator.isSellSignal(data, entryPrice, params);
+  }
+
+  @Override
+  public StrategyParameters getStrategyParameters() {
+    return StrategyParameters.builder()
+        .rsiBuyThreshold(50).rsiSellThreshold(50).rsiPeriod(14)
+        .macdShortBarCount(12).macdLongBarCount(26).macdBarCount(9)
+        .mfiPeriod(20).mfiOversoldThreshold(40).mfiOverboughtThreshold(50)
+        .lossPercent(5).profitPercent(5)
+        .build();
+  }
+}
